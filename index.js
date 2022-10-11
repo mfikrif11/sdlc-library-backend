925c6a4d4c241d48eb61d3ee11a0a72aea98d1d8
Muhammad Fikri Fernanda Yusuf <fernanda.fikri11@gmail.com>
2022-10-11 15:51:37 +0700

    main

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const db = require("./models")
+const cors = require("cors")
+const fs = require("fs")
+const handlebars = require("handlebars")
+
+dotenv.config()
+
+const PORT = 2000
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(PORT, async () => {
+    db.sequelize.sync({ alter: true })
+    console.log("Listening in Port", PORT)
+})
