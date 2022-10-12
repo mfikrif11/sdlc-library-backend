9e3e78816946f13f81aa301181c6361a1b022994
AndikaRB <111847058+AndikaRB@users.noreply.github.com>
2022-10-12 15:28:33 +0700

    register&Login (#2)

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const db = require("./models")
 const cors = require("cors")
 const fs = require("fs")
 const handlebars = require("handlebars")
+const authRoute = require("./routes/authRoute")
 const bookRoute = require("./routes/bookRoute")
+
 
 dotenv.config()
 
@@ -15,6 +17,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+
+app.use("/auth", authRoute)
 app.use("/books", bookRoute)
 
 app.listen(PORT, async () => {
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,36 @@
-const authController = require("../controllers/authController")
 const express = require("express")
+const { authController } = require("../controllers/authController")
+const { upload } = require("../lib/uploader")
+const { verifyToken } = require("../middlewares/authMiddleware")
+const { body } = require("express-validator")
+
 const router = express.Router()
 
-router.get("/", authController.getAllBooks)
+router.post(
+    "/register",
+    body(
+        "username",
+        "Username length has to be min 3, and only contain alphanumeric chars"
+    )
+        .isLength({ min: 3 })
+        .isAlphanumeric(),
+    body("email").isEmail(),
+    body("password",
+        "Password not strong enough"
+    ).isStrongPassword({
+        minLength: 8,
+        minNumbers: 1,
+        minUppercase: 1,
+        minSymbols: 1,
+        minLowercase: 1,
+    }),
+    authController.registerUser
+)
+
+router.post("/login", authController.loginUser)
+router.get("/refresh-token", verifyToken, authController.refreshToken)
+router.get("/verification", authController.verifyUser)
+router.post("/verification", verifyToken, authController.resendVerification)
 
 module.exports = router
+
