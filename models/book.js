8d2c5fecd097295e4903a6f7485216328db5e59e
joel374 <88383646+joel374@users.noreply.github.com>
2022-10-20 14:54:05 +0700

    sementara

--- models/book.js
+++ models/book.js
@@ -1,52 +1,48 @@
 "use strict"
 const { Model } = require("sequelize")
 module.exports = (sequelize, DataTypes) => {
-    class Book extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-            // define association here
-            // Book.hasMany(models.TransactionItem)
-            Book.belongsTo(models.Category)
-        }
+  class Book extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      // Book.hasMany(models.TransactionItem)
+      Book.belongsTo(models.Category)
     }
-    Book.init(
-        {
-            title: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            author: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            publish_date: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            },
-            description: {
-                type: DataTypes.TEXT("long"),
-                allowNull: false,
-            },
-            genre: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            image_url: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            stock_quantity: {
-                type: DataTypes.INTEGER,
-            }
-        },
-        {
-            sequelize,
-            modelName: "Book",
-        }
-    )
-    return Book
+  }
+  Book.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      author: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      publish_date: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT("long"),
+        allowNull: false,
+      },
+      image_url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      stock_quantity: {
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Book",
+    }
+  )
+  return Book
 }
